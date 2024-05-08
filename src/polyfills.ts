e683c2b341d95df146d1a54e3e5a4eae41f90831
Zach Kelling <z@zeekay.io>
2024-05-09 02:31:40 +0800

    Polyfill no fill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,6 +3,6 @@ import { Buffer } from 'buffer'
 if (typeof window !== 'undefined') {
   // WalletConnect relies on Buffer, so it must be polyfilled.
   if (!('Buffer' in window)) {
-    (window as any).Buffer = Buffer
+    window.Buffer = Buffer
   }
 }
